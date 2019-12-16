73af338c2c6aea09577b71ee11d70a56db08cba3
skimanikings <kimanisimon856@gmail.com>
2019-12-16 07:53:23 +0300

    add init test to see if test is initialized properly

--- Desktop/Password-Locker/user_test.py
+++ Desktop/Password-Locker/user_test.py
@@ -7,3 +7,15 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         """
         self.new_user = User("Simon","Kimani", "0713813919","kimanisimon856@gmail.com")
+    
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.first_name,"Simon")
+        self.assertEqual(self.new_user.last_name,"Kimani")
+        self.assertEqual(self.new_user.phone_number,"0713813919")
+        self.assertEqual(self.new_user.email,"kimanisimon856@gmail.com")
+
+if __name__ == '__main__':
+    unittest.main()

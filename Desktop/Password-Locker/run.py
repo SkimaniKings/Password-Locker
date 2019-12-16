2ca0a97b7d753745a205de58970a8a60e9cbc6c0
skimanikings <kimanisimon856@gmail.com>
2019-12-16 12:34:49 +0300

    create a function that will create a new user

--- Desktop/Password-Locker/run.py
+++ Desktop/Password-Locker/run.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3.6
 import credentials
 import user 
+def create_user(fname,lname,phone,email):
+    '''
+    Function to create a new user
+    '''
+    new_user = (fname,lname,phone,email)
+    return new_user

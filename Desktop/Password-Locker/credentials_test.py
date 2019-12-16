d3cf336d72d3d6e4b29d1f1a4684503982a7f3b7
skimanikings <kimanisimon856@gmail.com>
2019-12-16 10:33:50 +0300

    add setup instructions

---
+++ Desktop/Password-Locker/credentials_test.py
@@ -0,0 +1,8 @@
+import unittest
+from credentials import Credentials
+ def setUp(self):
+     """
+        Set up method to run before each test cases.
+     """
+     self.new_credentials_list=Credentials("twitter","kimsimo10@gmail.com","password")
+ 

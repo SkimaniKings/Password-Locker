22772bd8117d98bc873770891ba35c5c5bd23474
skimanikings <kimanisimon856@gmail.com>
2019-12-16 10:25:35 +0300

    add init method to pass prperties to our objects

--- Desktop/Password-Locker/credentials.py
+++ Desktop/Password-Locker/credentials.py
@@ -3,3 +3,7 @@ class Credentials:
     A class that generates new instances for users
     '''
     credentials_list=[]
+    def __init__(self,acc_name,acc_password):
+        self.acc_name=acc_name
+        self.acc_password=acc_password
+    

0fb6e5fff251fb44cba2ba04cf6ede30ec28846c
skimanikings <kimanisimon856@gmail.com>
2019-12-16 12:47:15 +0300

    add a function that creates and saves credentials

--- Desktop/Password-Locker/run.py
+++ Desktop/Password-Locker/run.py
@@ -12,3 +12,16 @@ def save_user(user):
     Function to save user
     """
     user.save_user_details()
+def create_credential(uname, pword, email):
+    """
+    Function to create new user credentials
+    """
+    new_credential = Credential(uname, pword, email)
+    return new_credential
+
+def save_credential(credential):
+    """
+    Function to save user credentials
+    """
+    credential.save_credential()
+    
